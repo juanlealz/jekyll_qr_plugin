7c98cdb8ccc2c17c589ff8c69376c79b20ded347
Juan Leal <juanlealz@gmail.com>
2023-01-01 21:32:13 +0100

    Inicial

---
+++ lib/jekyll_qr_plugin.rb
@@ -0,0 +1,6 @@
+require "jekyll_qr_plugin/version"
+
+module JekyllQrPlugin
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jekyll_qr_plugin/version.rb
@@ -0,0 +1,3 @@
+module JekyllQrPlugin
+  VERSION = "0.1.0"
+end
